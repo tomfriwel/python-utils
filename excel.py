43c318c87f75d8fc40b34b90ff79320c24e3905b
tomfriwel <976932447@qq.com>
2020-12-15 15:19:21 +0800

    add

---
+++ excel.py
@@ -0,0 +1,25 @@
+
+# $ pip install xlrd
+
+import xlrd
+
+def getExcelData(xlsxPath, keys):
+	xlsx = xlrd.open_workbook(xlsxPath)
+
+	table = xlsx.sheets()[0]
+
+	rowNum = table.nrows
+	colsNum = table.ncols
+	
+	rows = []
+	
+	for i in range(1, rowNum):
+		#获取每一行的数据
+		row = table.row_values(i)
+		
+		#把每一行的数据配合上面的key打成对象
+		rowObject = dict(zip(keys, row))
+		
+		rows.append(rowObject)
+		
+	return rows
